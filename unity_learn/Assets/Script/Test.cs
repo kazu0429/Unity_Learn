f997ce193e4705e4649f70256649e8ecede73260
Kazuki_Akimoto <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-02-05 19:31:15 +0900

    最初のコミット

---
+++ unity_learn/Assets/Script/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var test = "Test";
+        Debug.Log(test);
+        Debug.Log(test.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
