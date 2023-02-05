ae0c16d4604b9cf4603fc290603b64317e016aad
Kazuki_Akimoto <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-02-05 20:32:22 +0900

    コルーチン

--- unity_learn/Assets/Script/Test.cs
+++ unity_learn/Assets/Script/Test.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    private float startAt;
+    private IEnumerator testLoop;
+
     // Start is called before the first frame update
     void Start()
     {
-        var test = "Test";
-        Debug.Log(test);
-        Debug.Log(test.Length);
+        startAt = Time.realtimeSinceStartup;
+        testLoop = TestLoop();
+
+        StartCoroutine(testLoop);
+        //var test = "Test";
+        //Debug.Log(test);
+        //Debug.Log(test.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator TestLoop()
+    {
+        while (true)
+        {
+            var lifeTime = Time.realtimeSinceStartup - startAt;
+            Debug.Log("オブジェクトの生存時間（秒）: " + lifeTime);
+
+            if(lifeTime >= 30)
+            {
+                Debug.Log("TestLoopを停止します。");
+                yield break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
